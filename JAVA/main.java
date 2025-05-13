9762a737549c4b1f622f4b28a25d788ae9f8cf3e
Doneli ANDRIAMANANTSAINA <hei.marius.3@gmail.com>
2025-05-13 17:51:36 +0200

    feat:python update main file

--- JAVA/main.java
+++ JAVA/main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         ObjetLouable voiture = new ObjetLouable("Peugeot 208");
 
-        voiture.reserver(LocalDate.of(2025, 5, 13), 3); // OK
-        voiture.reserver(LocalDate.of(2025, 5, 15), 2); // Erreur - chevauchement
-        voiture.reserver(LocalDate.of(2025, 5, 17), 2); // OK
-        voiture.reserver(LocalDate.of(2025, 5, 17), 0); // Erreur - durée minimale
+        voiture.reserver(LocalDate.of(2025, 5, 13), 3);
+        voiture.reserver(LocalDate.of(2025, 5, 15), 2);
+        voiture.reserver(LocalDate.of(2025, 5, 17), 2);
+        voiture.reserver(LocalDate.of(2025, 5, 17), 0);
 
         voiture.afficherReservations();
     }
